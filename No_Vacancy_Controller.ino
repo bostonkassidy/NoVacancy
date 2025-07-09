8b1bd78b454aecd8b1c8547e2f770707695714c4
bostonkassidy <47906726+bostonkassidy@users.noreply.github.com>
2025-07-09 14:30:10 -0500

    Add files via upload

--- No_Vacancy_Controller.ino
+++ No_Vacancy_Controller.ino
@@ -3,10 +3,9 @@
 #include <Bounce2.h>
 #include <DYPlayerArduino.h>
 
-DY::Player player(&Serial1);
-
 
 int phoneID = 0;
+
 
 // Enter a MAC address and IP address for your controller below.
 // The IP address will be dependent on your local network:
@@ -23,19 +22,25 @@ IPAddress server(192, 168, 1, 165);  ////TOUCH PC IP
 // if you're using Processing's ChatServer, use port 10002):
 EthernetClient client;
 
+DY::Player player(&Serial1);
 
-// int service1 = 9;   // Service 1
-// int dnd1 = 10;  //DND1
-// int service2 = 11;
-// int dnd2 = 12;
-// int service3 = 13;
-// int dnd3 = 14;
+Bounce service1 = Bounce();
+Bounce service2 = Bounce();
+Bounce service3 = Bounce();
 
-const uint8_t relayPins[] = { 5, 6, 7, 8, 9, 10 };
+Bounce remote1 = Bounce();
+Bounce remote2 = Bounce();
+Bounce remote3 = Bounce();
+Bounce remote4 = Bounce();
+
+const uint8_t relayPins[] = { 6, 8, 10, 7, 9, 11};
 uint8_t relayState[6];
 
-const uint8_t buttonPins[] = { 2, 3, 4 };
+const uint8_t buttonPins[] = { 14, 15, 16 };
 uint8_t buttonState[3];
+
+const uint8_t remotePins[] = { 2, 3, 4, 5 };
+uint8_t remoteState[6];
 
 char messageArray[6];
 
@@ -63,7 +68,8 @@ void setup() {
   // Open serial communications and wait for port to open:
   Serial.begin(9600);
   player.begin();
-  player.setVolume(15);///50%
+  player.setVolume(25);///100%
+  player.setCycleMode(DY::PlayMode::OneOff); // Play all and repeat.
 
   pinMode(13, OUTPUT);
 
@@ -72,10 +78,23 @@ void setup() {
     //Serial.println("Setting pin " + String(i));
   }
 
-  for (int i = 0; i < 3; i++) {
-    pinMode(buttonPins[i], INPUT_PULLUP);
-    //Serial.println("Setting pin " + String(i));
-  }
+service1.attach( buttonPins[0] ,  INPUT_PULLUP );
+service2.attach( buttonPins[1] ,  INPUT_PULLUP );
+service3.attach( buttonPins[2] ,  INPUT_PULLUP );
+
+service1.interval(5); // interval in ms
+service2.interval(5); // interval in ms
+service3.interval(5); // interval in ms
+
+remote1.attach( remotePins[0] ,  INPUT_PULLUP );
+remote2.attach( remotePins[1] ,  INPUT_PULLUP );
+remote3.attach( remotePins[2] ,  INPUT_PULLUP );
+remote4.attach( remotePins[3] ,  INPUT_PULLUP );
+
+remote1.interval(5); // interval in ms
+remote2.interval(5); // interval in ms
+remote3.interval(5); // interval in ms
+remote4.interval(5);
 
 
   /////////////////////FLASH RELAYS AT STARTUP
@@ -129,39 +148,124 @@ void setup() {
 bool firstLoop = true;
 
 void loop() {
-      // if (client.connected()) {
-      //   client.print("ping");
-      //   Serial.print("sending message");
-      //   delay(1000);
-      // }
+service1.update();
+service2.update();
+service3.update();  
 
-  ///////BARTENDER BUTTONS Press
-  for (int i = 0; i < 3; i++) {
-    if (digitalRead(buttonPins[i]) == 0 && buttonState[i] == 0) {
-      Serial.println(String(i) + " pressed");     
-      delay(100);
-      if (client.connected()) {
-        client.print("0: " + String(i));
-        Serial.print("sending message");
+remote1.update();
+remote2.update();
+remote3.update();
+remote4.update();
+
+if(service1.changed()){
+  int serviceInput1 = service1.read();
+  if(serviceInput1 == LOW) {
+    if (client.connected()) {
+        client.print("0: 0");
+        Serial.println("0: 0");
       }
-
-      buttonState[i] = 1;
-
-    }
   }
+}
 
-
-
-/////////////Button Release
-  for (int i = 0; i < 3; i++) {
-    if (digitalRead(buttonPins[i]) == 1 && buttonState[i] == 1) {
-      buttonState[i] = 0;
-    }
+if(service2.changed()){
+  int serviceInput2 = service2.read();
+  if(serviceInput2 == LOW) {
+    if (client.connected()) {
+        client.print("0: 1");
+        Serial.println("0: 1");
+      }
   }
+}
+
+if(service3.changed()){
+  int serviceInput3 = service3.read();
+  if(serviceInput3 == LOW) {
+    if (client.connected()) {
+        client.print("0: 2");
+        Serial.println("0: 2");
+      }
+  }
+}
+
+if(remote1.changed()){
+  int remoteInput1 = remote1.read();
+  if(remoteInput1 == LOW) {
+    if (client.connected()) {
+        client.print("tv1:up");
+        Serial.println("tv1:up");
+      }
+  }
+}
+
+if(remote2.changed()){
+  int remoteInput2 = remote2.read();
+  if(remoteInput2 == LOW) {
+    if (client.connected()) {
+        client.print("tv1:down");
+        Serial.println("tv1:down");
+      }
+  }
+}
+
+if(remote3.changed()){
+  int remoteInput3 = remote3.read();
+  if(remoteInput3 == LOW) {
+    if (client.connected()) {
+        client.print("tv2:up");
+        Serial.println("tv2:up");
+      }
+  }
+}
+
+if(remote4.changed()){
+  int remoteInput4 = remote4.read();
+  if(remoteInput4 == LOW) {
+    if (client.connected()) {
+        client.print("tv2:down");
+        Serial.println("tv2:down");
+      }
+  }
+}
+
+  // ///////BARTENDER SERVICE BUTTONS Press
+  // for (int i = 0; i < 3; i++) {
+  //   if (digitalRead(buttonPins[i]) == 0 && buttonState[i] == 0) {
+  //     Serial.println(String(i) + " pressed");     
+  //     delay(10);
+  //     if (client.connected()) {
+  //       client.print("0: " + String(i));
+  //       Serial.print("sending message");
+  //     }
+  //     buttonState[i] = 1;
+  //   }
+  // }
+
+  // ///////BARTENDER REMOTE BUTTONS Press
+  // for (int i = 0; i < 4; i++) {
+  //   if (digitalRead(remotePins[i]) == 0 && remoteState[i] == 0) {
+  //     Serial.println(String(i) + " pressed");     
+  //     delay(10);
+  //     if (client.connected()) {
+  //       if(i==0){client.print("tv1:up" );}
+  //       if(i==1){client.print("tv1:down" );}
+  //       if(i==2){client.print("tv2:up" );}
+  //       if(i==3){client.print("tv2:down" );}
+  //     }
+  //     buttonState[i] = 1;
+  //   }
+  // }
+
+
+// /////////////Button Release
+//   for (int i = 0; i < 3; i++) {
+//     if (digitalRead(buttonPins[i]) == 1 && buttonState[i] == 1) {
+//       buttonState[i] = 0;
+//     }
+//   }
 
   if (relayPins[0] == 1 || relayPins[1] == 1 || relayPins[2] == 1) {
     /////BEEP and flash
-    player.playSpecified(1);
+    //player.playSpecified(1);
   }
 
 
@@ -191,6 +295,11 @@ void process_data(const char* data) {
 
   Serial.println("FROM TOUCH: " + incomingMessage);
 
+  if (incomingMessage == "beep"){
+    player.playSpecified(1);
+    player.play();
+  }
+
   if (incomingMessage[0] == '0') {
     Serial.println("updating lights");
     incomingMessage = incomingMessage.remove(0, 3);
@@ -203,8 +312,10 @@ void process_data(const char* data) {
       if (incomingMessage[i] == '1') {
         digitalWrite(relayPins[i], HIGH);
         Serial.println("Setting pin high: " + String(relayPins[i]));
+        relayState[i] = 1;
       } else {
         digitalWrite(relayPins[i], LOW);
+        relayState[i] = 0;
       }
       incomingMessage = incomingMessage.remove(i, 2);
     }
